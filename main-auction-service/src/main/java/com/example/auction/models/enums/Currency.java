89d62b34da3e622bd9100d4f2824afa8ec6e72d6
Timofei Belov <79749521+Tefaier@users.noreply.github.com>
2024-04-07 22:24:11 +0300

    local currency conversion logic

--- main-auction-service/src/main/java/com/example/auction/models/enums/Currency.java
+++ main-auction-service/src/main/java/com/example/auction/models/enums/Currency.java
@@ -1,7 +1,17 @@
 package com.example.auction.models.enums;
 
 public enum Currency {
-    RUB,
-    USD,
-    EUR
+    RUB(1),
+    USD(92.58f),
+    EUR(100.44f);
+
+    private final float comparativeValue;
+
+    Currency(float comparativeValue) {
+        this.comparativeValue = comparativeValue;
+    }
+
+    public float getComparativeValue() {
+        return comparativeValue;
+    }
 }
