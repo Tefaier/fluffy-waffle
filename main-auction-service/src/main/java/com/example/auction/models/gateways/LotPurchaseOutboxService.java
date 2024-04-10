4eacc742ba43911f1af6a25bc51028e61e4d96f0
Timofei Belov <79749521+Tefaier@users.noreply.github.com>
2024-04-10 19:07:17 +0300

    some more changes

--- main-auction-service/src/main/java/com/example/auction/models/gateways/LotPurchaseOutboxService.java
+++ main-auction-service/src/main/java/com/example/auction/models/gateways/LotPurchaseOutboxService.java
@@ -7,6 +7,9 @@ import com.example.auction.models.enums.LotState;
 import com.example.auction.models.services.LotService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gruelbox.transactionoutbox.TransactionOutbox;
+import com.gruelbox.transactionoutbox.TransactionOutboxEntry;
+import com.gruelbox.transactionoutbox.TransactionOutboxListener;
 import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +19,7 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.kafka.support.SendResult;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
@@ -25,23 +29,26 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 @Service
-public class LotPurchaseOutboxService {
+public class LotPurchaseOutboxService implements TransactionOutboxListener {
   private static final Logger LOGGER = LoggerFactory.getLogger(LotPurchaseOutboxService.class);
 
   private final KafkaTemplate<String, String> kafkaTemplate;
   private final String topicToSend;
   private final ObjectMapper objectMapper;
   private final LotService lotService;
+  private final TransactionOutbox outbox;
 
   @Autowired
   public LotPurchaseOutboxService(KafkaTemplate<String, String> kafkaTemplate,
                                   @Value("${topic-lot-purchase-request}") String topicToSend,
                                   ObjectMapper objectMapper,
-                                  LotService lotService) {
+                                  LotService lotService,
+                                  TransactionOutbox outbox) {
     this.kafkaTemplate = kafkaTemplate;
     this.topicToSend = topicToSend;
     this.objectMapper = objectMapper;
     this.lotService = lotService;
+    this.outbox = outbox;
   }
 
   public void pushRequestToKafka(LotPurchaseRequest request) throws JsonProcessingException {
@@ -71,5 +78,17 @@ public class LotPurchaseOutboxService {
     } catch (NoSuchElementException e) {
       acknowledgment.acknowledge();
     }
+  }
+
+  @Override
+  @Transactional
+  public void blocked(TransactionOutboxEntry entry, Throwable cause) {
+    TransactionOutboxListener.super.blocked(entry, cause);
+    lotService.getLot(((LotPurchaseRequest) entry.getInvocation().getArgs()[0]).userId()).setLotState(LotState.REJECTED);
+  }
+
+  @Scheduled(fixedDelayString = "${outbox-flush-frequency}")
+  protected void flushOutbox() {
+    outbox.flush();
   }
 }
