87dd07bfd85d7be760bbead3472f06b041571505
Timofei Belov <79749521+Tefaier@users.noreply.github.com>
2024-05-15 14:51:34 +0300

    removal of unused class

--- main-auction-service/src/main/java/com/example/auction/models/controllers/request/UserCreateRequest.java
+++
@@ -1,6 +0,0 @@
-package com.example.auction.models.controllers.request;
-
-import jakarta.validation.constraints.NotEmpty;
-
-public record UserCreateRequest(@NotEmpty String login, @NotEmpty String firstName, @NotEmpty String lastName, @NotEmpty String password, @NotEmpty String email) {
-}
