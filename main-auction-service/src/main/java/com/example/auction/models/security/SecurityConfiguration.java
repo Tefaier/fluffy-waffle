b4fc8dab10b997e90597c6bd7b93f0e69122ac32
Timofei Belov <79749521+Tefaier@users.noreply.github.com>
2024-05-16 17:53:07 +0300

    cors enabled through security configuration

--- main-auction-service/src/main/java/com/example/auction/models/security/SecurityConfiguration.java
+++ main-auction-service/src/main/java/com/example/auction/models/security/SecurityConfiguration.java
@@ -10,6 +10,11 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.NEVER;
 
@@ -27,7 +32,7 @@ public class SecurityConfiguration {
     return http
         .authorizeHttpRequests(auth ->
             auth.anyRequest().permitAll())
-        .cors(AbstractHttpConfigurer::disable)
+        .cors(cors -> cors.configure(http))
         .csrf(AbstractHttpConfigurer::disable)
         .formLogin(login ->
             login
@@ -41,5 +46,16 @@ public class SecurityConfiguration {
                 .logoutSuccessUrl("/login?logout=true")
                 .clearAuthentication(true))
         .build();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("*"));
+    configuration.setAllowedMethods(Arrays.asList("*"));
+    configuration.setAllowedHeaders(Arrays.asList("*"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 }
