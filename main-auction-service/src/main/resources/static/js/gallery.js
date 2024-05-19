2758d0d74ea28e572c8adb1e9c930a353e451a20
Ivan Anishchenko <ivananisenko08@gmail.com>
2024-05-19 20:53:06 +0300

    Images switch on lot page

--- main-auction-service/src/main/resources/static/js/gallery.js
+++ main-auction-service/src/main/resources/static/js/gallery.js
@@ -11,10 +11,17 @@ function updatedImage(id, shift) {
         }
         image.src = imageUrls[currentIndex];
         cardsInfo[id].currentIndex = currentIndex;
-
+        return;
     }
-}
-
-function updateRight(id) {
-
+    const gallery = document.querySelector('.description__gallery');
+    if (gallery) {
+        const image = gallery.querySelector('.description__image');
+        let currentIndex = cardsInfo[id].currentIndex;
+        const imageUrls = cardsInfo[id].imageUrls;
+        if (-1 < currentIndex + shift && currentIndex + shift < imageUrls.length) {
+            currentIndex += shift;
+        }
+        image.src = imageUrls[currentIndex];
+        cardsInfo[id].currentIndex = currentIndex;
+    }
 }
