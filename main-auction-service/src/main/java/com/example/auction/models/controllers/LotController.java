d0b5a51866469c57e617c404d7d28f48d7e77b96
Timofei Belov <79749521+Tefaier@users.noreply.github.com>
2024-05-20 10:33:18 +0300

    top bet endpoint

--- main-auction-service/src/main/java/com/example/auction/models/controllers/LotController.java
+++ main-auction-service/src/main/java/com/example/auction/models/controllers/LotController.java
@@ -1,18 +1,17 @@
 package com.example.auction.models.controllers;
 
+import com.example.auction.models.DTOs.DTOMoney;
 import com.example.auction.models.DTOs.LotCreateRequest;
 import com.example.auction.models.DTOs.LotDto;
 import com.example.auction.models.entities.Lot;
 import com.example.auction.models.entities.Money;
-import com.example.auction.models.entities.User;
+import com.example.auction.models.services.BetService;
 import com.example.auction.models.services.LotService;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,10 +20,12 @@ import java.util.stream.Collectors;
 //@PreAuthorize("isAuthenticated()")
 public class LotController {
   private final LotService lotService;
+  private final BetService betService;
 
   @Autowired
-  public LotController(LotService lotService) {
+  public LotController(LotService lotService, BetService betService) {
     this.lotService = lotService;
+    this.betService = betService;
   }
 
   @PostMapping()
@@ -53,5 +54,16 @@ public class LotController {
   public LotDto getLotById(@NotNull @PathVariable("id") Long lotId) {
     Lot lot = lotService.getLot(lotId);
     return lot.toLotDto();
+  }
+
+  @GetMapping("/{id}/top")
+  @Transactional
+  public DTOMoney getTopBet(@NotNull @PathVariable("id") Long lotId) {
+    var bet = betService.getHighestValueBet(lotService.getLot(lotId).getLotBets());
+    return bet == null ? null : new DTOMoney(
+        bet.getValue().getIntegerPart(),
+        bet.getValue().getDecimalPart(),
+        bet.getValue().getCurrency()
+    );
   }
 }
